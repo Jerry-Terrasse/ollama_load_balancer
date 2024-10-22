52d7af4ae8538828ee2287332d96133a770bb684
Ronen Zyroff <rzyroff@gmail.com>
2024-10-22 19:49:28 +0300

    We don't have enough information to determine whether an error occurred

--- src/main.rs
+++ src/main.rs
@@ -293,17 +293,11 @@ impl Drop for ServerGuard {
         let mut servers_lock = self.servers.lock().unwrap();
         if let Some(server) = servers_lock.get_mut(&self.key) {
             server.state.busy = false;
-            match server.state.failure_record {
-                FailureRecord::Reliable => {
-                    println!("🟢 Server {} now available", self.key);
-                },
-                FailureRecord::Unreliable => {
-                    println!("⚠️  Server {} now available but still not reliable", self.key);
-                },
-                FailureRecord::SecondChanceGiven => {
-                    // If failure_record == SecondChanceGiven that means an error message has already been printed
-                    // so this error message would be confusing indeed.
-                },
+            if matches!(server.state.failure_record, FailureRecord::Reliable) {
+                println!("🟢 Server {} now available", self.key);
+            }
+            else {
+                println!("⚠️  Server {} now available but still not reliable", self.key);
             }
         }
     }
